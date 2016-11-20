e1f7a83be9e69a53599a4588133f4a3bd07167de
fumimi <info@fumimi.jp>
2016-11-20 03:57:53 +0000

    Viewを使って文字列を表示

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,8 @@
 class HelloController < ApplicationController
-    def index
-        render text: "こんにちは、世界！"
+    # def index
+    #     render text: "こんにちは、世界！"
+    # end
+    def view
+        @msg = "こんにちは、世界！"
     end
 end
