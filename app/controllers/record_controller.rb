a2b62d12cc8d6fb28f747f2b1a64fca09b87f5f1
fumimi <info@fumimi.jp>
2016-11-30 12:28:33 +0000

    修正

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -10,12 +10,20 @@ class RecordController < ApplicationController
     end
 
 
-def ph1
- @books = Book.where('publish = ? AND price >= ?',
- params[:publish], params[:price])
- render 'hello/list'
-end
+    def ph1
+        @books = Book.where('publish = ? AND price >= ?',
+        params[:publish], params[:price])
+        render 'hello/list'
+    end
 
+    def find
+     @books = Book.find([2, 5, 10])
+     render 'hello/list'
+    end
     
+    def find_by
+        @book = Book.find_by(publish: '技術評論社')
+        render 'books/show'
+    end
 
 end
